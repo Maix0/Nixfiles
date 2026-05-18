640becb20a1592b3c9373c77c9808b7ff31d2dad
Maix0 <39835848+Maix0@users.noreply.github.com>
2026-05-18 16:20:25 +0200

    feat(hyprpowerctl): reordered and changed way stuff is done

--- packages/hyprpowerctl/hyprpowerctl.nix
+++ packages/hyprpowerctl/hyprpowerctl.nix
@@ -12,18 +12,28 @@
     packageName = "hyprpowerctl";
   in {
     packages.${packageName} = let
-      mkOption = icon: name: {inherit icon name;};
-      options = {
-        shutdown = mkOption "⏻" "Shutdown";
-        restart = mkOption "" "Restart";
-        logout = mkOption "󰗽" "Logout";
-        sleep = mkOption "󰤄" "Sleep";
-        lock = mkOption "" "Lock";
-      };
+      mkOption = icon: name: action: {inherit icon name action;};
+      options = [
+        (mkOption "" "Lock" "${lib.getExe pkgs.hyprlock}")
+        (mkOption "󰗽" "Logout" "${lib.getExe pkgs.hyprshutdown} -t \"Confirm logout...\"")
+        (mkOption "󰤄" "Sleep" "${lib.getExe' pkgs.systemd "systemctl"} suspend")
+        (mkOption "⏻" "Shutdown" "${lib.getExe pkgs.hyprshutdown} -t \"Confirm shutdown...\" -p \"shutdown -P 0\"")
+        (mkOption "" "Restart" "${lib.getExe pkgs.hyprshutdown} -t \"Confirm reboot...\" -p \"reboot\"")
+      ];
       mkChoice = {
         icon,
         name,
+        action,
       }: " ${icon}  ${name}";
+      mkAction = {
+        icon,
+        name,
+        action,
+      } @ opts: ''
+        "${mkChoice opts}")
+          ${action};
+          ;;
+      '';
     in
       pkgs.writeShellApplication {
         name = "hyprpowerctl";
@@ -34,24 +44,10 @@
         #  Lock
 
         text = ''
-          RESULT="$(printf "${mkChoice options.shutdown}\n${mkChoice options.restart}\n${mkChoice options.logout}\n${mkChoice options.sleep}\n${mkChoice options.lock}" | ${lib.getExe pkgs.hyprlauncher} --dmenu)";
+          RESULT="$(printf "${builtins.concatStringsSep "\\n" (builtins.map mkChoice options)}" | ${lib.getExe pkgs.hyprlauncher} --dmenu)";
 
           case "$RESULT" in
-            "${mkChoice options.shutdown}")
-              ${lib.getExe pkgs.hyprshutdown} -t "Confirm shutdown..." -p "shutdown -P 0";
-              ;;
-            "${mkChoice options.restart}")
-              ${lib.getExe pkgs.hyprshutdown} -t "Confirm reboot..." -p "reboot";
-              ;;
-            "${mkChoice options.logout}")
-              ${lib.getExe pkgs.hyprshutdown} -t "Confirm logout...";
-              ;;
-            "${mkChoice options.sleep}")
-              ${lib.getExe' pkgs.systemd "systemctl"} suspend;
-              ;;
-            "${mkChoice options.lock}")
-              ${lib.getExe pkgs.hyprlock};
-              ;;
+            ${builtins.concatStringsSep "\n" (map mkAction options)}
           esac
         '';
       };
